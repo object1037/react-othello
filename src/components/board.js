425960200060196b7d87e7863ba3889058a002e0
object1037 <contact@object1037.dev>
2021-05-12 19:32:22 +0900

    add move validation feature

--- src/components/board.js
+++ src/components/board.js
@@ -9,11 +9,39 @@ initArr[28] = initArr[35] = 2;
 export default function Board(props) {
   const [cells, setCells] = useState(initArr)
   
+  function validateMove(cell) {
+    if (cells[cell] !== 0) return false;
+    let enemyColor = props.blackIsNext ? 1 : 2;
+    let myColor = props.blackIsNext ? 2 : 1;
+    const horizonMoves = [-1, 1];
+    const verticalMoves = [-8, 8];
+    for (let i = 0; i < 2; i++) {
+      let horizon = cell + horizonMoves[i];
+      let vertical = cell + verticalMoves[i];
+      while (cells[horizon] === enemyColor) {
+        horizon += horizonMoves[i];
+        if (cells[horizon] === myColor) {
+          return true;
+        }
+      }
+      while (cells[vertical] === enemyColor) {
+        vertical += verticalMoves[i];
+        if (cells[vertical] === myColor) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  
   function handleCellChange(cell) {
-    const cellsCopy = cells.slice()
-    cellsCopy[cell] = props.blackIsNext ? 2 : 1
-    setCells(cellsCopy)
-    props.setBlackIsNext(!props.blackIsNext)
+    console.log(cell)
+    if (validateMove(cell)) {
+      const cellsCopy = cells.slice()
+      cellsCopy[cell] = props.blackIsNext ? 2 : 1
+      setCells(cellsCopy)
+      props.setBlackIsNext(!props.blackIsNext)
+    }
   }
   return (
     <div className="board">
